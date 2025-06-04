4a176b42535d710bb39ed5bf5e2eb0ced2605944
Isaac Sales <124395470+IsaacSales15@users.noreply.github.com>
2025-06-04 15:41:44 -0300

    cubo preto

---
+++ cubopreto_obi/cubo.java
@@ -0,0 +1,34 @@
+package cubopreto_obi;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class cubo {
+
+  public static void cubo(double n) {
+
+    double dois = 2;
+
+    int f0 = (int) Math.pow((n - dois), 3.0);
+    System.out.println("Zero faces: " + f0);
+    int f2 = (int) ((n - 2) * 12);
+    System.out.println("Duas faces: " + f2);
+    int f3 = 8;
+    System.out.println("Três faces: " + f3);
+    int f1 = (int) (Math.pow(n, 3.0) - (f2 + f3 + f0));
+    System.out.println("Uma face: " + f1);
+
+  }
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    var n = scanner.nextDouble();
+
+    if (n >= 2 || n <= 100) {
+      cubo(n);
+    }
+
+    scanner.close();
+  }
+}
